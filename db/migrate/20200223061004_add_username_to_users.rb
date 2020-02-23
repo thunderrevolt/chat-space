0cdd4f588d773b6a290cca4a44a7d8cf47a7c5e6
thunderrevolt <61190252+thunderrevolt@users.noreply.github.com>
2020-02-23 19:25:45 +0900

    user登録機能　じっし

---
+++ db/migrate/20200223061004_add_username_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string, null: false, default: ""
+    add_index :users, :name, unique: true
+  end
+end
