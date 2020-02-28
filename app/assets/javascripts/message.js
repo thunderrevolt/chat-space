3a548cc4130e1b2628afe255cb021a94003def07
thunderrevolt <61190252+thunderrevolt@users.noreply.github.com>
2020-02-28 18:49:34 +0900

    チャット自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function(){
-
   function buildHTML(message){
     if (message.image) {
       var html = 
-          `<div class="message__info">
+        `<div class="message", data-message-id=${message.id}>
+          <div class="message__info">
             <div class="message__info--user">
               ${message.user_name}
             </div>
@@ -16,11 +16,13 @@ $(function(){
               ${message.content}
             </p>
             <img class="lower-message__image" src=${message.image} alt="Thumb flexbox cheat sheet">
-          </div>`
+          </div>
+        </div>`
       return html;
     } else {
       var html = 
-             `<div class="message__info">
+          `<div class="message", data-message-id=${message.id}>
+             <div class="message__info">
                 <div class="message__info--user">
                   ${message.user_name}
                 </div>
@@ -32,12 +34,12 @@ $(function(){
                 <p class="lower-message__content">
                   ${message.content}
                 </p>
-              </div>`
+              </div>
+          </div>`
       return html;
     }
   }
   
-
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -52,7 +54,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.message').append(html);
+      $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('#new_message > input.form__message--submit').attr('disabled', false)
@@ -61,4 +63,29 @@ $(function(){
       alert("メッセージ送信に失敗しました")
     })
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
